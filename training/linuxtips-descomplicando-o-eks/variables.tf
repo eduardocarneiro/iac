206839ff3e9098751d7f572795b570cd8b6b6a52
Eduardo Carneiro <eduardo.oc@gmail.com>
2025-10-29 20:59:49 -0300

    added the internet gateway

--- training/linuxtips-descomplicando-o-eks/variables.tf
+++ training/linuxtips-descomplicando-o-eks/variables.tf
@@ -2,6 +2,10 @@ variable "project_name" {
   type = string
 }
 
+variable "igw_name" {
+  type = string
+}
+
 variable "region" {
   type = string
 }
@@ -15,3 +19,14 @@ variable "vpc_additional_cidrs" {
   type        = list(string)
   description = "Additional VPC"
 }
+
+/*
+variable "public_subnets" {
+  description = "List of Public subnets"
+  type = list(object({
+    name              = string
+    cidr              = string
+    availability_zone = string
+  }))
+}
+*/
--- training/linuxtips-descomplicando-o-eks/vpc.tf
+++ training/linuxtips-descomplicando-o-eks/vpc.tf
@@ -1,11 +1,11 @@
 resource "aws_vpc" "main" {
-    cidr_block = var.vpc_cidr
-    enable_dns_hostnames = true
-    enable_dns_support = true
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
-    tags = {
-      Name = var.project_name
-    }
+  tags = {
+    Name = var.project_name
+  }
 }
 
 // doc aws CIDR blocks
@@ -14,6 +14,6 @@ resource "aws_vpc" "main" {
 resource "aws_vpc_ipv4_cidr_block_association" "main" {
   count = length(var.vpc_additional_cidrs)
 
-  vpc_id = aws_vpc.main.id
+  vpc_id     = aws_vpc.main.id
   cidr_block = var.vpc_additional_cidrs[count.index]
 }
