02c05879e9505f776c5000da6cf7c6580a0b7526
Eduardo Carneiro <eduardo.oc@gmail.com>
2025-12-26 18:55:31 -0300

    added parameter_store.tf outputs.tf and initial setup for aws eks vanilla

---
+++ training/linuxtips-descomplicando-o-eks/02-aws-eks-vanilla/variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  type = string
+}
+
+variable "eks_name" {
+  type = string
+}
+
+variable "ssm_vpc" {
+  type = string
+}
+
+variable "ssm_public_subnets" {
+  type = list(string)
+}
+
+variable "ssm_private_subnets" {
+  type = list(string)
+}
+
+variable "ssm_pod_subnets" {
+  type = list(string)
+}
