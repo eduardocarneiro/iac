dd6d72813fbfac199ed3c5a6ef24e6510f26c4f6
Eduardo Carneiro <eduardo.oc@gmail.com>
2025-12-29 19:44:03 -0300

    added a metrics server and kube-state-metrics

---
+++ training/linuxtips-descomplicando-o-eks/02-aws-eks-vanilla/helm_kube_state_metrics.tf
@@ -0,0 +1,30 @@
+resource "helm_release" "kube_state_metrics" {
+  name             = "kube-state-metrics"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "kube-state-metrics"
+  namespace        = "metrics-server"
+  create_namespace = true
+
+  set = [ {
+    name = "apiService.create"
+    value = "true"
+  },
+  {
+    name = "metricLabelsAllowlist[0]"
+    value = "nodes=[*]"
+  },
+  {
+    name  = "metricLabelsAllowlist[0]"
+    value = "nodes=[*]"
+  },
+  {
+    name  = "metricAnnotationsAllowList[0]"
+    value = "nodes=[*]"
+  }
+   ]
+
+  depends_on = [
+    aws_eks_cluster.main,
+    aws_eks_node_group.main
+  ]
+}
