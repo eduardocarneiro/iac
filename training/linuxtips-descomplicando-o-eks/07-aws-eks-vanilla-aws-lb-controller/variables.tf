70c02df6c10fe82922afb07661752a2db0274e3a
Eduardo Carneiro <eduardo.oc@gmail.com>
2026-01-02 13:06:58 -0300

    added initial setup - aws-load-balancer-controller

---
+++ training/linuxtips-descomplicando-o-eks/07-aws-eks-vanilla-aws-lb-controller/variables.tf
@@ -0,0 +1,58 @@
+variable "region" {
+  type = string
+}
+
+variable "eks_name" {
+  type = string
+}
+
+variable "ssm_vpc" {
+  type = string
+}
+
+variable "ssm_public_subnets" {
+  type = list(string)
+}
+
+variable "ssm_private_subnets" {
+  type = list(string)
+}
+
+variable "ssm_pod_subnets" {
+  type = list(string)
+}
+
+variable "kms_name" {
+  type = string
+}
+
+variable "k8s_version" {
+  type = string
+}
+
+variable "auto_scale_options" {
+  type = object({
+    min     = number
+    max     = number
+    desired = number
+  })
+}
+
+variable "nodes_instance_sizes" {
+  type = list(string)
+}
+
+variable "addon_cni_version" {
+  type    = string
+  default = "v1.20.4-eksbuild.2"
+}
+
+variable "addon_coredns_version" {
+  type    = string
+  default = "v1.11.3-eksbuild.1"
+}
+
+variable "addon_kubeproxy_version" {
+  type    = string
+  default = "v1.31.10-eksbuild.12"
+}
