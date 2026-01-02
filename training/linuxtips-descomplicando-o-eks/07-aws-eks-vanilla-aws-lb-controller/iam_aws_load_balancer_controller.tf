877fb7f0ed564b58a447b8f8437ee48f125e3455
Eduardo Carneiro <eduardo.oc@gmail.com>
2026-01-02 15:50:01 -0300

    changed the namespace in IAM

--- training/linuxtips-descomplicando-o-eks/07-aws-eks-vanilla-aws-lb-controller/iam_aws_load_balancer_controller.tf
+++ training/linuxtips-descomplicando-o-eks/07-aws-eks-vanilla-aws-lb-controller/iam_aws_load_balancer_controller.tf
@@ -7,7 +7,7 @@ data "aws_iam_policy_document" "aws_lb_role" {
     condition {
       test     = "StringEquals"
       variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
-      values   = ["system:serviceaccount:kube-system:aws-load-balancer-controller"]
+      values   = ["system:serviceaccount:aws-lb-controller:aws-load-balancer-controller"]
     }
 
     principals {
