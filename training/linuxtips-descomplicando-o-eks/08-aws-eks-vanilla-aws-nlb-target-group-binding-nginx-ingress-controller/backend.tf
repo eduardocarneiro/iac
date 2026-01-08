eac4922191ef17814758278a56dcf566aad6ffad
Eduardo Carneiro <eduardo.oc@gmail.com>
2026-01-08 19:42:20 -0300

    added target group binding by kubectl_manifest resource

--- training/linuxtips-descomplicando-o-eks/08-aws-eks-vanilla-aws-nlb-target-group-binding-nginx-ingress-controller/backend.tf
+++ training/linuxtips-descomplicando-o-eks/08-aws-eks-vanilla-aws-nlb-target-group-binding-nginx-ingress-controller/backend.tf
@@ -2,16 +2,4 @@ terraform {
   backend "s3" {
 
   }
-
-  required_providers {
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-    }
-
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = "1.9.1"
-    }
-  }
-
 }
--- training/linuxtips-descomplicando-o-eks/08-aws-eks-vanilla-aws-nlb-target-group-binding-nginx-ingress-controller/providers.tf
+++ training/linuxtips-descomplicando-o-eks/08-aws-eks-vanilla-aws-nlb-target-group-binding-nginx-ingress-controller/providers.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.19.0"
+    }
+  }
+}
+
+provider "kubectl" {
+  host                   = aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.default.token
+}
+
 provider "aws" {
   region = var.region
 }
@@ -16,3 +31,4 @@ provider "helm" {
   }
 
 }
+
