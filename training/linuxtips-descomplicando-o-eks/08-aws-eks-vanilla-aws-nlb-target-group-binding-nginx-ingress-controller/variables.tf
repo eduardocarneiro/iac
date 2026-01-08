b3c51aea8ebc06b06abfedb9e54520ee741e01b8
Eduardo Carneiro <eduardo.oc@gmail.com>
2026-01-08 15:23:36 -0300

    added aws eks cluster using aws nlb target-group-binding and nginx ingress controller

---
+++ training/linuxtips-descomplicando-o-eks/08-aws-eks-vanilla-aws-nlb-target-group-binding-nginx-ingress-controller/variables.tf
@@ -0,0 +1,75 @@
+variable "region" {
+  type = string
+}
+
+variable "eks_name" {
+  type = string
+}
+
+variable "ssm_vpc" {
+  type = string
+}
+
+variable "ssm_public_subnets" {
+  type = list(string)
+}
+
+variable "ssm_private_subnets" {
+  type = list(string)
+}
+
+variable "ssm_pod_subnets" {
+  type = list(string)
+}
+
+variable "kms_name" {
+  type = string
+}
+
+variable "k8s_version" {
+  type = string
+}
+
+variable "auto_scale_options" {
+  type = object({
+    min     = number
+    max     = number
+    desired = number
+  })
+}
+
+variable "nodes_instance_sizes" {
+  type = list(string)
+}
+
+variable "addon_cni_version" {
+  type    = string
+  default = "v1.20.4-eksbuild.2"
+}
+
+variable "addon_coredns_version" {
+  type    = string
+  default = "v1.11.3-eksbuild.1"
+}
+
+variable "addon_kubeproxy_version" {
+  type    = string
+  default = "v1.31.10-eksbuild.12"
+}
+
+variable "aws_lb_controller_name" {
+  type = string
+}
+
+variable "aws_lb_policy_name" {
+  type = string
+}
+
+variable "nlb_name" {
+  type = string
+}
+
+variable "nlb_target_group_name" {
+  type = string
+}
+
